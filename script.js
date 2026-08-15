8bb28cf849e5bd4df3711a5092353983d7518723
Areeba ayoub <155888760+Areebaayoub@users.noreply.github.com>
2026-08-15 16:36:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+
 const apikey = "96631abe744d769ba798bcd9da45711d";
 
 const searchBox = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
 const weatherIcon = document.querySelector(".weather-icon"); 
-
 
 async function checkWeather(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apikey}`;
@@ -14,29 +14,29 @@ async function checkWeather(city) {
 
         const data = await response.json();
 
-        
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-        
         const weather = data.weather[0].main.toLowerCase(); 
 
-        if (weather === "cloudy") {
-            weatherIcon.src = "Images/cloudy.png";
+        // Removed "Images/" from all paths and fixed "clouds" bug
+        if (weather === "clouds") {
+            weatherIcon.src = "cloudy.png";
         } else if (weather === "clear") {
-            weatherIcon.src = "Images/clear.png";
+            weatherIcon.src = "clear.png";
         } else if (weather === "rain") {
-            weatherIcon.src = "Images/rain.png";
+            weatherIcon.src = "rain.png";
         } else if (weather === "drizzle") {
-            weatherIcon.src = "Images/drizzle.png";
+            weatherIcon.src = "drizzle.png";
         } else if (weather === "mist") {
-            weatherIcon.src = "Images/mist.png";
+            weatherIcon.src = "mist.png";
         } 
         else if (weather === "snow") {
-            weatherIcon.src = "Images/snow.png";
+            weatherIcon.src = "snow.png";
         } 
+        
         document.querySelector(".weather").style.display = "block";
 
     } catch (error) {
@@ -44,7 +44,6 @@ async function checkWeather(city) {
         alert("City not found. Try again.");
     }
 }
-
 
 searchButton.addEventListener("click", () => {
     const city = searchBox.value.trim();
@@ -56,6 +55,7 @@ searchButton.addEventListener("click", () => {
 });
 
 checkWeather("Karachi");
+
 searchBox.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         searchButton.click();
